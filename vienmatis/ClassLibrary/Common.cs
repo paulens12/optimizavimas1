8b4e27527cf452a885309830dbae540daf0c7243
Paulius <pauliusval99@hotmail.com>
2019-09-24 23:29:43 +0300

    niutono metodas

--- vienmatis/ClassLibrary/Common.cs
+++ vienmatis/ClassLibrary/Common.cs
@@ -13,14 +13,18 @@ namespace ClassLibrary
     public static class Common
     {
         private const string FloatFormat = "G17";
+        private const double a = 0;
+        private const double b = 8;
 
         public static double F(double x)
         {
-            double a = 0;
-            double b = 8;
             double r = x * x - a;
             return r * r / b - 1;
         }
+
+        public static double Fprime1(double x) => 4 * x * (x * x - a) / b;
+
+        public static double Fprime2(double x) => 4 * (3 * x * x - a) / b;
 
         public static void AddVerticalLine(this PlotModel pm, double x, string name)
         {
---
+++ vienmatis/niutono_metodas/View.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ClassLibrary;
+using OxyPlot;
+using OxyPlot.Series;
+
+namespace niutono_metodas
+{
+    public class View
+    {
+        public View()
+        {
+            this.MyModel = new PlotModel { Title = "Niutono metodas" };
+            this.MyModel.Series.Add(new FunctionSeries(Common.F, 0, 10, 0.001, "generuojanti funkcija"));
+            //this.MyModel.AddVerticalLine(2.245, "test");
+
+            Console.WriteLine(dalink(5, Common.Fprime1, Common.Fprime2, 0.0001, 1));
+        }
+
+        private double dalink(double x, Func<double, double> f1, Func<double, double> f2, double e, int step)
+        {
+            double xNew = x - f1(x) / f2(x);
+
+            this.MyModel.AddVerticalLine(xNew, step.ToString());
+
+            if (Math.Abs(xNew - x) < e)
+            {
+                return xNew;
+            }
+
+            return dalink(xNew, f1, f2, e, step + 1);
+        }
+
+        public PlotModel MyModel { get; private set; }
+    }
+}
